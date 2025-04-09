0507a7bdc6a78c0e0949a651aed92777e4f5b012
Kkiirraan <kiranvijayanv@gmail.com>
2025-04-09 22:58:06 +0530

    Kiran(AuthController)

---
+++ CaptoneProject.Services.AuthAPI/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using CaptoneProject.Services.AuthAPI.User;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaptoneProject.Services.AuthAPI.Data
+{
+    public class AppDbContext: IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
