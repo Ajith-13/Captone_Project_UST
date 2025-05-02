75b56ff97093b66e81563b49923ff43e06cfcb40
Kkiirraan <103481327+Kkiirraan@users.noreply.github.com>
2025-05-02 12:46:45 +0530

    Delete CaptoneProject.Services.CourseAPI directory

--- CaptoneProject.Services.CourseAPI/Data/Dto/Course/CourseDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CaptoneProject.Services.CourseAPI.Data.Dto.Course
-{
-    public class CourseDto
-    {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public IFormFile ThumbnailImage { get; set; }
-    }
-}
--- CaptoneProject.Services.CourseAPI/Data/Dto/ResponseDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CaptoneProject.Services.CourseAPI.Data.Dto
-{
-    public class ResponseDto
-    {
-        public bool IsSuccess { get; set; }
-        public object? Data { get; set; }
-        public string Message { get; set; }
-    }
-}
